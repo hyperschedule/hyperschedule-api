18fd2994053d9cbcdaedfa12ed557e0a94875ccc
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-09-05 19:37:54 -0700

    remove space before semicolon

--- server.py
+++ server.py
@@ -25,8 +25,8 @@ import traceback
 
 def unique(lst):
     new_lst = []
-    for item in lst :
-        if item not in new_lst :
+    for item in lst:
+        if item not in new_lst:
             new_lst.append(item)
     return new_lst
 
