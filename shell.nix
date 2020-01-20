e2fbbcd13d86319261da35a4d21a5500df06104d
Kye W. Shi <kwshi@hmc.edu>
2020-01-20 12:57:41 -0800

    create Nix shell environment for building scraper

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  chromeAlias = pkgs.stdenv.mkDerivation {
+    name = "google-chrome";
+    phases = ["buildPhase"];
+    buildPhase = ''
+      mkdir -p $out/bin
+      ln -s ${pkgs.google-chrome}/bin/google-chrome-stable $out/bin/google-chrome
+    '';
+  };
+in
+pkgs.mkShell {
+  buildInputs = 
+    [chromeAlias] ++ (with pkgs; [
+      chromedriver
+    ]) ++ (with pkgs.python37Packages; [
+      beautifulsoup4
+      flask
+      gunicorn
+      lxml
+      dateutil
+      selenium
+      requests
+      psutil
+      frozendict
+      flask-cors
+      atomicwrites
+      boto3
+      flake8
+    ]);
+}
