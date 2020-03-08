58bbb04aab45d391c97df1905326f30f5aba4b02
ssantichaivekin <jsantichaivekin@g.hmc.edu>
2020-03-07 16:43:55 -0800

    Create upload-syllabus endpoint
    Create upload syllabus endpoint that reads token from post request and get the user corresponding to the token from Firebase.

--- hyperschedule/app.py
+++ hyperschedule/app.py
@@ -98,6 +98,22 @@ def view_api_v3():
         raise APIError("data not available yet")
     return api_response({"data": diff, "until": until, "full": full})
 
+@app.route("/upload-syllabus", methods = ['POST'])
+@nocache
+def upload_syllabus():
+    """
+    A post method to upload course syllabus. The POST method requires a token
+    and a syllabus pdf. It uploads the syllabus to Firebase and return success
+    or failure.
+    """
+    token = flask.request.json.get("token")
+    if not token:
+        raise APIError("request failed to provide token")
+    user = firebase_admin.auth.verify_id_token(token)
+    # TODO: read syllabus to post method
+    # TODO: implement syllabus upload to Firebase and its local copy
+    raise NotImplementedError("Upload Syllabus feature not implemented")
+
 
 app.worker = worker.HyperscheduleWorker()
 app.worker.start()
